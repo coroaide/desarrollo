e40ac4fe6b554209c0ce9724eb775dd34832460d
coroaide <113808517+coroaide@users.noreply.github.com>
2024-03-14 09:00:17 -0400

    Add files via upload
    modificacion 1

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './src/components/Navigation';
+import { AuthProvider } from './src/context/AuthContext';
+
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <Navigation/>
+    </AuthProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
